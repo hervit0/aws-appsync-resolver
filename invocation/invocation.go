afbf293aef732d3a9e737a61a31d570f6d007fb3
Hervito <herve.ahleung@gmail.com>
2019-10-19 13:13:20 +0100

    HAL > Implement resolver call

--- invocation/invocation.go
+++ invocation/invocation.go
@@ -11,8 +11,8 @@ type Invocation struct {
 // https://docs.aws.amazon.com/appsync/latest/devguide/resolver-context-reference.html
 // https://docs.aws.amazon.com/appsync/latest/devguide/tutorial-lambda-resolvers.html
 type Context struct {
-	Arguments *json.RawMessage `json:"arguments"` // AWS Documentation: A map containing all GraphQL arguments for this field
-	Source    *json.RawMessage `json:"source"`    // AWS Documentation: A map containing the resolution of the parent field.
+	Arguments *json.RawMessage `json:"arguments"` // AWS Doc: A map containing all GraphQL arguments for this field
+	Source    *json.RawMessage `json:"source"`    // AWS Doc: A map containing the resolution of the parent field.
 	Result    *json.RawMessage `json:"result"`
 	Identity  *json.RawMessage `json:"identity"`
 	Request   *json.RawMessage `json:"request"`
--- resolver/resolver.go
+++ resolver/resolver.go
@@ -3,6 +3,8 @@ package resolver
 import (
 	"context"
 	"encoding/json"
+	"fmt"
+	"reflect"
 )
 
 type Request *json.RawMessage
@@ -16,8 +18,23 @@ type Resolver struct {
 }
 
 func (resolver Resolver) Resolve(context context.Context, request Request) (Response, error) {
-	// TODO: Map the argument and perform the call
-	//reflect.ValueOf(resolver.handler).Call(payload)
-	var response Response
-	return response, nil
+	handlerRequestType := reflect.TypeOf(resolver.Handler).In(1)
+	requestArguments, err := parse(request, &handlerRequestType)
+	if err != nil {
+		return nil, err
+	}
+	requestArgumentsWithContext := append([]reflect.Value{reflect.ValueOf(context)}, requestArguments...)
+
+	response := reflect.ValueOf(resolver.Handler).Call(requestArgumentsWithContext)
+	return response[0].Interface(), response[1].Interface().(error)
+}
+
+func parse(rawPayload Request, requestType *reflect.Type) ([]reflect.Value, error) {
+	args := reflect.New(*requestType)
+
+	if err := json.Unmarshal(*rawPayload, args.Interface()); err != nil {
+		return nil, fmt.Errorf("error parsing payload: %v", err)
+	}
+
+	return append([]reflect.Value{}, args.Elem()), nil
 }
